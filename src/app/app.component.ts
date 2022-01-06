26b89bacffda677fcd3a004785a031b6a2e33444
jonacuru <jcuspoca@qvision.com.co>
2022-01-06 11:57:01 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'holaMundo';
+
+  texto : string = '"El diligenciamiento del Formulario SARLAFT está pendiente por gestionar, por favor ingrese al siguiente enlace[$LINK$]https:\/\/sarlaft.labsura.com\/redirect\/iniciar-proceso\/CA684055AA56454B4A6755A108FEDD261DE1F68A316B10D22A0A6401CC114B15","Se';
+
+  textoLimpio : string = this.texto.replace('\/','/');
+
+  expresion = /([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#]?[\w-]+)*\/?/gm;
+
+  direccion = this.textoLimpio.match(this.expresion);
+
 }
